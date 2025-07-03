24b289b05d034725506d843c4c5a256cef2865f4
bearpixelmedia <bearpixelmedia@gmail.com>
2025-07-02 23:18:15 -0400

    Update Vercel build configuration in package.json and vercel.json - Remove CI=false from vercel-build script and add buildCommand to ensure consistent build behavior during deployment.

---
+++ client/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest'],
+  rules: {
+    'jsx-a11y/heading-has-content': 'off',
+    'no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+}; 
